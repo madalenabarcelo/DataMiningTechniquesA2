70a919b32e6d6e33363091e488f35d25c586c0d6
Rik <rikvan.dijk@externals.adidas.com>
2025-05-06 14:32:34 +0200

    Refactor LGBMClassifierModel: Enhance data formatting and evaluation methods
    - Updated format_data method to accept target and group parameters for flexibility.
    - Introduced get_X method to streamline feature matrix extraction.
    - Modified train_val_split to return validation indices for better tracking.
    - Enhanced evaluate method to compute NDCG score alongside ROC AUC.
    - Added methods for NDCG calculation, including mean_ndcg_at_k and dcg.
    - Improved prediction handling by adding predictions directly to DataFrame.
    - Cleaned up code for clarity and maintainability.

--- data_preparation.py
+++ data_preparation.py
@@ -6,7 +6,19 @@ import seaborn as sns
 
 class DataPreparer:
     def __init__(self):
-        pass
+        # These are the columns where nan has some meaning
+        self.flag_columns = (
+            ["visitor_hist_starrating", "visitor_hist_adr_usd", "prop_review_score", 
+            "srch_query_affinity_score", "orig_destination_distance"]
+            + [f"comp{i}_rate" for i in range(1, 9)]
+            + [f"comp{i}_inv" for i in range(1, 9)]
+            + [f"comp{i}_rate_percent_diff" for i in range(1, 9)]
+        )
+        # In these columns we will impute the null values with either the mean, median or whatever (to be decided)
+        self.impute_null_columns = [] 
+        
+        # These are the columns where zero has some meaning
+        self.flag_zero_columns = ["prop_review_score", "prop_log_historical_price"]
 
     def load_and_preprocess_data(self, old_file_name: str, new_file_name:str) -> None:
         """
@@ -14,12 +26,21 @@ class DataPreparer:
         """
         # Load the data
         df = self.load_data(old_file_name)
+
+        # Handle date and time columns and add features related to it
+        df = self.handle_date_time(df)
+
+        # Cast all non-numeric columns to numeric
+        df = self.cast_to_numeric(df)
         
         # Shrink data types before processing to make it faster
         df = self.shrink_data_types(df)
 
-        # Handle date and time columns and add features related to it
-        df = self.handle_date_time(df)
+        # Add flag columns for null and zeros to the DataFrame
+        df = self.handle_missing_values(df)
+
+        # Add features to the DataFrame
+        df = self.add_features(df)
 
         # Shrink data types again after processing to save memory
         df = self.shrink_data_types(df)
@@ -30,6 +51,19 @@ class DataPreparer:
     def load_data(self,old_file_name:str, folder:str="data") -> pl.DataFrame:
         path = folder + "/" + old_file_name
         return pl.read_csv(path, low_memory=False, null_values=["NA", "N/A", "null", "NULL", "NaN"])
+    
+    def cast_to_numeric(self, df: pl.DataFrame) -> pl.DataFrame:
+        """Cast all non-numeric columns to numeric. First try to cast to int8, int16, then to float32."""
+        for col in df.columns:
+            if (df[col].dtype == pl.Object) or (df[col].dtype == pl.Utf8):
+                try:
+                    df = df.with_columns(pl.col(col).cast(pl.Int8))
+                except Exception:
+                    try:
+                        df = df.with_columns(pl.col(col).cast(pl.Int16))
+                    except Exception:
+                        df = df.with_columns(pl.col(col).cast(pl.Float32))
+        return df
     
     def shrink_data_types(self, df: pl.DataFrame) -> pl.DataFrame:
         """
@@ -50,14 +84,68 @@ class DataPreparer:
             (pl.col("date_time").dt.hour().is_between(12, 18)).cast(pl.Int8).alias("is_afternoon"),
             (pl.col("date_time").dt.hour().is_between(18, 24)).cast(pl.Int8).alias("is_evening"),
             (pl.col("date_time").dt.hour().is_between(0, 6)).cast(pl.Int8).alias("is_night"),
+            (pl.col("date_time").dt.month().is_between(3, 5)).cast(pl.Int8).alias("is_spring"),
             (pl.col("date_time").dt.month().is_between(6, 8)).cast(pl.Int8).alias("is_summer"),
             (pl.col("date_time").dt.month().is_between(9, 11)).cast(pl.Int8).alias("is_autumn"),
-            (pl.col("date_time").dt.month().is_between(12, 2)).cast(pl.Int8).alias("is_winter"),
-            (pl.col("date_time").dt.month().is_between(3, 5)).cast(pl.Int8).alias("is_spring"),
+            (pl.col("date_time").dt.month().is_in([1,2,12])).cast(pl.Int8).alias("is_winter"),
             (pl.col("date_time").dt.weekday().is_between(5, 6)).cast(pl.Int8).alias("is_weekend"),
         ])
         
         return df.drop("date_time")
+    
+    def handle_missing_values(self, df: pl.DataFrame) -> pl.DataFrame:
+        """
+        Add flag columns to the DataFrame. The flag columns are created by checking if the values in the specified columns are null.
+        For selected columns, the null values are imputed with the mean of the column grouped by "srch_id".
+        The zero values in the specified columns are also flagged and replaced with NaN.
+        """
+        for col in self.flag_columns:
+            # Create a flag column for each column in the list
+            df = df.with_columns((pl.col(col).is_null()).cast(pl.Int8).alias(f"{col}_flag"))
+
+        if len(self.impute_null_columns) > 0:
+            # For the columns where we want to impute the null values, we use the mean grouped by "srch_id" and replace
+            for col in self.impute_null_columns:
+                # Do with mean for now
+                df_mean = df.group_by("srch_id").agg([
+                    pl.col(col).mean().alias(f"{col}_mean")
+                ])
+            # Merge the mean values back to the original DataFrame
+            df = df.join(df_mean, on="srch_id", how="left")
+
+            for col in self.impute_null_columns:
+                # Impute the null values with the mean values
+                df = df.with_columns(
+                    pl.when(pl.col(col).is_null())
+                    .then(pl.col(f"{col}_mean"))
+                    .otherwise(pl.col(col))
+                    .alias(col)
+                ).drop(f"{col}_mean")
+
+        for col in self.flag_zero_columns:
+            # Create a flag column for each column in the list
+            df = df.with_columns((pl.col(col) == 0).cast(pl.Int8).alias(f"{col}_zero_flag"))
+            # From what I see the zero value could be interpreted by the model as a value rather than a missing data flag
+            # Therefore it makes sense to replace the zero value with nan
+            df = df.with_columns(pl.when(pl.col(col) == 0).then(None).otherwise(pl.col(col)).alias(col))
+            # Also for both current columns imputing it does not make sense so keep it nan
+
+        ### Consider what to do with nan in original columns (see notes)
+        ### Consider what to do with prop_starrating (see notes)
+        return df
+    
+    def add_features(self, df: pl.DataFrame) -> pl.DataFrame:
+        """
+        Add features to the DataFrame.
+        """
+        df = df.with_columns([
+            pl.col("srch_id").count().over("srch_id").alias("result_count"),
+            pl.when(pl.col("visitor_location_country_id") == pl.col("prop_country_id"))
+            .then(1).otherwise(0).alias("prop_in_visitor_country"),
+        ])
+
+        
+        return df
 
     def upload_data(self, df: pl.DataFrame, file_name: str, folder: str = "data") -> None:
         """
--- lgbm_classifier.py
+++ lgbm_classifier.py
@@ -8,27 +8,33 @@ from sklearn.metrics import roc_auc_score
 class LGBMClassifierModel:
     def __init__(self, **kwargs):
         self.model = LGBMClassifier(**kwargs)
+        # Categorical features should be passed explicitly to the model, these do not include the indentifying columns (srch_id, prop_id)
+        self.categorical_features = ["site_id", "visitor_location_country_id", "prop_country_id", "srch_destination_id"]
 
     def format_and_train(self, train_df:pl.DataFrame) -> None:
         """Format the training data and train the model."""
         # Format the data
-        X_train, y_train, X_val, y_val = self.format_data(train_df)
+        X_train, y_train, X_val, y_val, val_idx = self.format_data(train_df)
 
         # Fit the model
         self.fit(X_train, y_train, X_val, y_val)
+        return X_train, y_train, X_val, y_val, val_idx
 
-    def format_data(self, df:pl.DataFrame) -> tuple[np.array, np.array, np.array, np.array]:
+    def format_data(self, df:pl.DataFrame, target:str = "click_bool", group: str = "srch_id") -> tuple[np.array, np.array, np.array, np.array, np.array]:
         """Format the data for LightGBM."""
-        feature_cols = self.get_feature_cols(df)
-
-        # Extract feature matrix, target array, and query‐groups
-        X = df.select(feature_cols).to_numpy()
-        y = df["click_bool"].to_numpy()
-        groups = df["srch_id"].to_numpy()
+        X = self.get_X(df)
+        y = df[target].to_numpy()
+        groups = df[group].to_numpy()
 
         # Split the data into training and validation sets
-        X_train, y_train, X_val, y_val = self.train_val_split(X, y, groups)
-        return X_train, y_train, X_val, y_val
+        X_train, y_train, X_val, y_val, val_idx = self.train_val_split(X, y, groups)
+        return X_train, y_train, X_val, y_val, val_idx
+    
+    def get_X(self, df:pl.DataFrame) -> np.array:
+        """Get the feature matrix from the DataFrame."""
+        feature_cols = self.get_feature_cols(df)
+        X = df.select(feature_cols).to_numpy()
+        return X
 
     def get_feature_cols(self, df:pl.DataFrame) -> list[str]:
         """Get feature columns from the DataFrame. The training set contains multiple columns that are not in the test set."""
@@ -36,14 +42,14 @@ class LGBMClassifierModel:
             "srch_id",     # group/key for queries
             "prop_id",     # identifier
             "click_bool",  # target
-            "booking_bool",# only used for NDCG grading later
-            "gross_booking_usd",
+            "booking_bool", # only in train
+            "gross_bookings_usd", # only in train
             "position",    # only in train
         }
         feature_cols = [c for c in df.columns if c not in to_exclude]
         return feature_cols
     
-    def train_val_split(self, X:np.array, y:np.array, groups:np.array) -> tuple[np.array, np.array, np.array, np.array]:
+    def train_val_split(self, X:np.array, y:np.array, groups:np.array) -> tuple[np.array, np.array, np.array, np.array, np.array]:
         """Split the data into training and validation sets."""
         # Create a group‐aware train/validation split
         gkf = GroupKFold(n_splits=5)
@@ -51,7 +57,7 @@ class LGBMClassifierModel:
         train_idx, val_idx = next(gkf.split(X, y, groups))
         X_train, X_val = X[train_idx], X[val_idx]
         y_train, y_val = y[train_idx], y[val_idx]
-        return X_train, y_train, X_val, y_val
+        return X_train, y_train, X_val, y_val, val_idx
     
     def fit(self, X_train:np.array, y_train:np.array, X_val:np.array, y_val:np.array, eval_metric: str = "auc", 
             early_stopping_rounds:int = 50, verbose:int = 20) -> None:
@@ -61,29 +67,82 @@ class LGBMClassifierModel:
             eval_set=[(X_val, y_val)],
             eval_metric=eval_metric,
             callbacks=[early_stopping(early_stopping_rounds), log_evaluation(verbose)],
+            categorical_feature=self.categorical_features,
         )
 
-    def evaluate(self, X_val:np.array, y_val:np.array) -> float:
+    def evaluate_validation(self, X_val:np.array, y_val:np.array, train_df: pl.DataFrame, val_idx) -> float:
         """Evaluate the model."""
+        # Get the ROC AUC score
+        roc_auc = self.get_roc_auc(X_val, y_val)
+
+        val_df:pl.DataFrame = train_df[val_idx]
+
+        # Compute NDCG score
+        mean_ndcg = self.get_ndcg_score(val_df)
+        return roc_auc, mean_ndcg
+    
+    def get_ndcg_score(self, df:pl.DataFrame, k:int = 5) -> float:
+        """Get the NDCG score."""
+        df = self.add_predictions(df)
+        df = df.select(["srch_id", "click_bool", "score"])
+        df = df.sort(["srch_id", "score"], descending=[False, True])
+
+        # Compute NDCG@k
+        mean_ndcg = self.mean_ndcg_at_k(df, k=k)
+        print(f"Validation NDCG@{k}:", mean_ndcg)
+        return mean_ndcg
+
+    def mean_ndcg_at_k(self, df:pl.DataFrame, k:int=5):
+        """Compute mean NDCG@k across all srch_ids"""
+        ndcgs = []
+        for srch_id, group in df.group_by("srch_id"):
+            # Relevance is typically click_bool or booking_bool (or a combined score)
+            relevances = group["click_bool"].to_list()  # or use combined click + 5*booking
+            ndcgs.append(self.ndcg_at_k(relevances, k))
+        return np.mean(ndcgs)
+    
+    def ndcg_at_k(self, relevances: list[float], k: int) -> float:
+        """Normalized DCG at rank k"""
+        ideal = sorted(relevances, reverse=True)
+        ideal_dcg = self.dcg(ideal, k)
+        return self.dcg(relevances, k) / ideal_dcg if ideal_dcg > 0 else 0.0
+    
+    def dcg(self, relevances: list[float], k: int) -> float:
+        """Discounted Cumulative Gain at rank k"""
+        relevances = np.array(relevances)[:k]
+        return np.sum((2 ** relevances - 1) / np.log2(np.arange(2, len(relevances) + 2)))
+    
+    def get_roc_auc(self, X_val: np.array, y_val:np.array) -> float:
+        """Get the ROC AUC score."""
         y_val_pred = self.model.predict_proba(X_val)[:,1]
         roc_auc = roc_auc_score(y_val, y_val_pred)
         print("Validation AUC:", roc_auc)
         return roc_auc
+
+    def add_predictions(self,df:pl.DataFrame) -> pl.DataFrame:
+        """Add predictions to a DataFrame."""
+        predictions = self.predict(df)
+        val_df = self.add_predictions_to_df(df, predictions)
+        return val_df
     
-    def get_predictions(self, test_df:pl.DataFrame) -> np.array:
+    def add_predictions_to_df(self, df:pl.DataFrame, predictions:np.array) -> pl.DataFrame:
+        """Add predictions to the DataFrame."""
+        df = df.with_columns(pl.Series(name="score", values=predictions))
+        return df.sort(["srch_id", "score"], descending=[False, True])
+
+    def get_final_predictions(self, df:pl.DataFrame) -> np.array:
         """Get the property predictions for the test set."""
-        predictions = self.predict(test_df)
-        prop_predictions = self.get_prop_predictions(test_df, predictions)
+        predictions = self.predict(df)
+        prop_predictions = self.get_prop_predictions(df, predictions)
         return prop_predictions
 
-    def predict(self, test_df:pl.DataFrame) -> np.array:
+    def predict(self, df:pl.DataFrame) -> np.array:
         """Generate predictions on the test set."""
-        feature_cols = self.get_feature_cols(test_df)
-        X_test = test_df.select(feature_cols).to_numpy()
-        return self.model.predict_proba(X_test)[:,1]
+        X = self.get_X(df)
+        return self.model.predict_proba(X)[:,1]
     
-    def get_prop_predictions(self, test_df:pl.DataFrame, predictions:np.array) -> pl.DataFrame:
+    def get_prop_predictions(self, df:pl.DataFrame, predictions:np.array) -> pl.DataFrame:
         """Get property predictions."""
-        test_df = test_df.with_columns(score = pl.Series(predictions))
-        return test_df.sort(["srch_id", "score"], reversed=[False, True]).select(["srch_id", "prop_id"])
+        df = df.with_columns(score = pl.Series(predictions))
+        return df.sort(["srch_id", "score"], descending=[False, True]).select(["srch_id", "prop_id"])
         
